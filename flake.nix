5a00b667458ec1c50b3773635d5726fdc70c13b1
Vortriz <vorarishi22+github@gmail.com>
2026-04-07 15:25:25 +0530

    init

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+    description = "A very basic flake";
+
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+        systems.url = "github:nix-systems/x86_64-linux";
+    };
+
+    outputs = {
+        nixpkgs,
+        systems,
+        ...
+    }: let
+        inherit (nixpkgs) lib;
+        pkgsFor = lib.genAttrs (import systems) (system: import nixpkgs {inherit system;});
+        forEachSystem = f: lib.genAttrs (import systems) (system: f pkgsFor.${system});
+    in {
+        formatter = forEachSystem (pkgs:
+            pkgs.treefmt.withConfig {
+                runtimeInputs = [pkgs.nixfmt];
+                settings.formatter.nixfmt = {
+                    command = "nixfmt";
+                    includes = ["*.nix"];
+                    options = ["--indent=4"];
+                };
+            });
+
+        devShells = forEachSystem (pkgs: {
+            default = pkgs.mkShell {
+                packages = with pkgs; [
+                    typst
+                    typstyle
+                ];
+            };
+        });
+    };
+}
---
+++ globals.typ
@@ -0,0 +1,16 @@
+#let underline-color = rgb("#91cbfd")
+#let icon-color = underline-color.darken(20%)
+#let link-color = underline-color.mix(rgb("#4d8ecf"))
+
+#let pipe-sep = [#h(4pt) | #h(4pt)]
+
+#let place(it) = text(weight: "regular", size: 0.9em, fill: rgb("#7c7c7c"), it)
+
+#let LaTeX = {
+  set text(font: "New Computer Modern", weight: "bold")
+  let a = text(baseline: -0.35em, size: 0.66em, "A")
+  let e = text(baseline: 0.22em, "E")
+  box(
+    "L" + h(-0.32em) + a + h(-0.13em) + "T" + h(-0.14em) + e + h(-0.14em) + "X",
+  )
+}
